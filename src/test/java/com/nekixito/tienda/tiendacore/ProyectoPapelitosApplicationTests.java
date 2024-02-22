d65afb2c849677b5f3dfcf99eedae62a6078c3ac
Miguel Nequiz <nekixito@gmail.com>
2024-02-21 19:38:37 -0600

    Inicio de proyecto de papelitos

---
+++ src/test/java/com/nekixito/tienda/tiendacore/ProyectoPapelitosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nekixito.tienda.tiendacore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoPapelitosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
